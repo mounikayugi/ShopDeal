57dfd13593c2a97fb20f6a9f1f22ee874a22855c
mounikayugi <158800455+mounikayugi@users.noreply.github.com>
2024-09-26 17:10:09 +0530

    Update NewCollections.jsx

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -7,7 +7,7 @@ const NewCollections = () => {
      const [new_collection, setNew_collection] = useState([]);
 
      useEffect(()=>{
-        fetch('https://upgrad-capstone-project-backend.onrender.com')
+        fetch('https://upgrad-capstone-project-backend.onrender.com/newcollections')
         .then((response)=>response.json())
         .then((data)=>setNew_collection(data));
      },[])
